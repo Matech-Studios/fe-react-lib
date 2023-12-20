1df7b0189151ca251f78c8262212ef518054ab3a
Emanuel Ceballos <eceballos@matechstudios.com>
2023-12-20 19:27:20 -0300

    Added publish step to continuous deployment pipeline

--- jest.config.ts
+++ jest.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
         '^.+\\.(css|less|sass)$': 'babel-jest',
         '\\.svg$': '<rootDir>/svg-name-mapper.js'
     },
-    modulePathIgnorePatterns: ['dist'],
+    modulePathIgnorePatterns: ['lib'],
     modulePaths: ['<rootDir>'],
     preset: 'ts-jest',
     rootDir: '.',
