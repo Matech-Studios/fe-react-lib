3520aafd9ba5c552bf5a07ba20985fa266424ae4
Emanuel Ceballos <eceballos@matechstudios.com>
2023-12-25 23:54:33 -0300

    Uncomment 1st test

--- src/utils/domain/envVars.test.ts
+++ src/utils/domain/envVars.test.ts
@@ -2,9 +2,8 @@ import { EnvVars } from 'src/utils/domain';
 
 describe('envVars', () => {
     it('should return process.env variables when available', () => {
-        // process.env.FIREBASE_API_KEY = 'firebase-api-key';
-        // expect(EnvVars.getVars().FIREBASE_API_KEY).toBe('firebase-api-key');
-        expect(true).toBe(true);
+        process.env.FIREBASE_API_KEY = 'firebase-api-key';
+        expect(EnvVars.getVars().FIREBASE_API_KEY).toBe('firebase-api-key');
     });
 
     /// global.window.__RUNTIME_CONFIG__ is set in jest.testsSetup.ts
