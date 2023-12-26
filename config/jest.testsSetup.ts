44a5d08bd5ea099924873c3ae81816a099bf8361
Parzival <2164846+emanuelceballos@users.noreply.github.com>
2023-12-26 00:03:21 -0300

    Fix process not defined error. Added tests for envVars. (#15)
    * Fix process not defined error. Added tests for envVars.
    
    * __RUNTIME_CONFIG__ defined in tests setup
    
    * Added --forceExit to try to exit even if firebase locks resources
    
    * Added logs
    
    * Re-added missing .eslint. Added more logging.
    
    * Try watch=false, added missing dev dependencies
    
    * Try watch=false, added missing dev dependencies
    
    * Comment envVars tests
    
    * Uncomment 1st test
    
    * Set undefined process.env instead of process
    
    * Remove redundant 'yarn' and 'yarn build'

--- config/jest.testsSetup.ts
+++ config/jest.testsSetup.ts
@@ -28,16 +28,15 @@ export const useAuthMock = {
     confirmResetPassword: jest.fn(),
     applyFirebaseActionCode: jest.fn()
 };
-const envVars = {
-    API_BASE_URL: '',
-    ATTENDEE_BASE_URL: '',
-    FIREBASE_API_KEY: '',
-    FIREBASE_AUTH_DOMAIN: '',
-    FIREBASE_PROJECT_ID: '',
-    FIREBASE_STORAGE_BUCKET: '',
-    FIREBASE_MESSAGING_SENDER_ID: '',
-    FIREBASE_APP_ID: '',
-    FIREBASE_MEASUREMENT_ID: ''
-};
 
-Object.defineProperty(window, '__RUNTIME_CONFIG__', { value: envVars });
+const envVars = { 
+    FIREBASE_API_KEY: 'firebase-api-key', 
+    FIREBASE_AUTH_DOMAIN: 'firebase-auth-domain', 
+    FIREBASE_PROJECT_ID: 'firebase-project-id', 
+    FIREBASE_STORAGE_BUCKET: 'firebase-storage-bucket', 
+    FIREBASE_MESSAGING_SENDER_ID: 'firebase-messaging-sender-id', 
+    FIREBASE_APP_ID: 'firebase-app-id', 
+    FIREBASE_MEASUREMENT_ID: 'firebase-measurement-id' 
+}; 
+ 
+Object.defineProperty(window, '__RUNTIME_CONFIG__', { value: envVars }); 
